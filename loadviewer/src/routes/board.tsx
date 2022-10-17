8af85b8941ee9512069cb7d3d8fc1de6716161fc
Avimitin <avimitin@gmail.com>
2022-10-17 18:19:41 +0800

    Use the data utils
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- loadviewer/src/routes/board.tsx
+++ loadviewer/src/routes/board.tsx
@@ -1,10 +1,15 @@
 import { LoaderFunctionArgs, useLoaderData } from "react-router-dom"
+import { GetMachineById, Machine } from "../data";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  return params.boardId;
+  if (params.boardId === undefined) {
+    throw new Error("No board id found in params")
+  }
+  const id = parseInt(params.boardId);
+  return GetMachineById(id);
 }
 
 export default function Board() {
-  const board_id = useLoaderData();
-  return (<h3><p>Place Holder for {`${board_id}`}</p></h3>);
+  const board = useLoaderData() as Machine;
+  return (<h3><p>Board: {board.name}</p></h3>);
 }
