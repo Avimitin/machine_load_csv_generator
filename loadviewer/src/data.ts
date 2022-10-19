2e6fad566fb67b84d950ab4b411f650f5d668d60
Avimitin <avimitin@gmail.com>
2022-10-19 16:20:19 +0800

    Remove fake data source
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- loadviewer/src/data.ts
+++ loadviewer/src/data.ts
@@ -1,54 +1,3 @@
-interface FakeCache {
-  [index: string]: boolean
-}
-
-let fakeCache: FakeCache = {};
-const fakeMachines: Array<Machine> = [
-  { id: 1, name: "magmortar" },
-  { id: 2, name: "larvesta" },
-  { id: 3, name: "feltchinder" },
-];
-
-const fakeRecords1: Array<Record> = [
-  { ttime: new Date(2022, 10, 18, 14, 8, 32), users: 8, load: 3.71 },
-  { ttime: new Date(2022, 10, 19, 15, 8, 32), users: 7, load: 1.45 },
-  { ttime: new Date(2022, 10, 20, 16, 8, 32), users: 10, load: 4.83 },
-  { ttime: new Date(2022, 10, 21, 17, 8, 32), users: 10, load: 6.23 },
-];
-
-const fakeRecords2: Array<Record> = [
-  { ttime: new Date(2022, 10, 18, 14, 8, 32), users: 8, load: 2.32 },
-  { ttime: new Date(2022, 10, 19, 15, 8, 32), users: 8, load: 8.88 },
-  { ttime: new Date(2022, 10, 20, 16, 8, 32), users: 8, load: 0.32 },
-  { ttime: new Date(2022, 10, 21, 17, 8, 32), users: 8, load: 0.61 },
-];
-
-const fakeStorage: { [index: number]: Array<Record> } = {
-  1: fakeRecords1,
-  2: fakeRecords2,
-}
-
-async function fakeNetwork(key: string | null) {
-  if (key === null) {
-    fakeCache = {};
-    return;
-  }
-
-  if (fakeCache[key] !== undefined) {
-    return;
-  }
-
-  fakeCache[key] = true;
-  return new Promise(res => {
-    setTimeout(res, Math.random() * 800);
-  });
-}
-
-export interface Machine {
-  id: number,
-  name: string,
-}
-
 export interface Record {
   ttime: Date,
   users: number,
@@ -73,26 +22,6 @@ function parseCSV(s: string): Record[] {
 export interface GitHubRootDirResponse {
   path: string,
   download_url: string,
-}
-
-export async function getMachineById(id: number) {
-  await fakeNetwork(`boardId:${id}`);
-
-  const mach = fakeMachines.find(machine => machine.id === id);
-  if (mach === undefined) {
-    throw new Error(`Machine ${id} not found`)
-  }
-  return mach ?? null;
-}
-
-export async function getLoadsByMid(id: number) {
-  await fakeNetwork(`records:${id}`);
-  const record = fakeStorage[id];
-  if (record === undefined) {
-    throw new Error(`Machine ${id} not found`);
-  }
-
-  return record;
 }
 
 export async function getLoadsByMachName(mname: string) {
