32994697c49daf6f0782459685eef584c3339827
Avimitin <avimitin@gmail.com>
2022-10-21 15:48:37 +0800

    Move form to a single component
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- loadviewer/src/routes/root.tsx
+++ loadviewer/src/routes/root.tsx
@@ -60,6 +60,38 @@ function NavBar({ ghfile }: { ghfile: GhFileResp }) {
   );
 }
 
+function SearchForm(props: { q: string, searching?: boolean, submit: SubmitFunction }) {
+  const { q, searching, submit } = props;
+  return (
+    <div>
+      <Form id="search-form" role="search">
+        <input
+          id="q"
+          aria-label="Search contacts"
+          placeholder="Search"
+          type="search"
+          name="q"
+          defaultValue={q}
+          className={searching ? "loading" : ""}
+          onChange={(event) => {
+            const isFirstSearch = q === null;
+            submit(event.currentTarget.form, { replace: !isFirstSearch });
+          }}
+        />
+        <div
+          id="search-spinner"
+          aria-hidden
+          hidden={!searching}
+        />
+        <div
+          className="sr-only"
+          aria-live="polite"
+        ></div>
+      </Form>
+    </div>
+  )
+}
+
 export default function Root() {
   const { q } = useLoaderData() as data;
   const navigation = useNavigation();
@@ -85,32 +117,7 @@ export default function Root() {
     <>
       <div id="sidebar">
         <h1>Unmatched Status</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-              defaultValue={q}
-              className={searching ? "loading" : ""}
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget.form, { replace: !isFirstSearch });
-              }}
-            />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={!searching}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
-          </Form>
-        </div>
+        <SearchForm q={q} searching={searching} submit={submit} />
         <NavBar
           ghfile={{
             data: data,
